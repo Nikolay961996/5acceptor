3455635a411bad92a78427a327998e4da596d2ec
aphilome <1.katrin.1@mail.ru>
2024-11-30 00:29:42 +0300

    start bot project

---
+++ bot/Polumna007/Polumna007/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+Console.WriteLine("Nigora hello");
+
+
+//using Telegram.Bot;
+
+//var token = "7457083066:AAHXkUwQLcfq5U1z9e2YlJHslGGMfvlb1oQ";
+//var bot = new TelegramBotClient(token);
+//var me = await bot.GetMe();
+//Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
